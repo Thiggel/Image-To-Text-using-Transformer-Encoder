4068d958ab329e95309c33c930720256aa751186
F.M.De.Sousa.Horta.Osorio.Laitenberger@student.rug.nl <s3894479@peregrine.hpc.rug.nl>
2022-03-17 15:25:39 +0100

    .

--- datasets/visualgenome/VisualGenomeQuestionsAnswers.py
+++ datasets/visualgenome/VisualGenomeQuestionsAnswers.py
@@ -5,6 +5,7 @@ from torchtext.data.utils import get_tokenizer
 from PIL import Image
 from os.path import join, exists
 from torch import tensor, Tensor
+from torchvision.transforms import RandomHorizontalFlip, RandomVerticalFlip, ColorJitter, RandomAffine, ToPILImage
 
 from datasets.ImageTextDataset import ImageTextDataset
 
@@ -16,10 +17,15 @@ class VisualGenomeQuestionsAnswers(ImageTextDataset):
             images_part1_dir: str,
             images_part2_dir: str,
             questions_answers_file: str,
+            patch_size: int = 16,
+            image_size: int = 128,
             transform: Optional[Callable] = None
     ) -> None:
+        self.image_size = image_size
+
         self.tokenizer = get_tokenizer('basic_english')
         self.data = self.load_questions(questions_answers_file)
+        self.num_images = len(self.data)
 
         super().__init__(sentence_list=self.word_list())
 
@@ -33,9 +39,30 @@ class VisualGenomeQuestionsAnswers(ImageTextDataset):
 
         self.transform = transform
 
+        # if there is a patch embedding in the network that is trained on
+        # this set, a patch size can be defined so that the image datapoints
+        # are augmented, being shifted by up to patch_size-1 pixels in each
+        # direction
+        self.patch_size = patch_size
+
+        # for data augmentation, a certain number of transforms
+        # are performed on each image. A corresponding
+        # array of transforms is initialized hereafter
+        self.augmentations = self.init_augmentations()
+
         # there is as many classes as words in the vocabulary
         # as an answer to a question can be any one word
         self.num_classes = self.vocab_size
+	
+        from os import mkdir
+        from os.path import exists
+        if not exists('testimgs'): mkdir('testimgs')
+	
+        for i in range(len(self.augmentations)):
+            index = i * len(self.data)
+            ToPILImage()(self.__getitem__(index)[0][0]).save('testimgs/' + str(index) + '.jpg')
+            ToPILImage()(self.__getitem__(index + 1)[0][0]).show('testimgs/' + str(index + 1) + '.jpg')
+            ToPILImage()(self.__getitem__(index + 2)[0][0]).show('testimgs/' + str(index + 2) + '.jpg')
 
     def preprocess_answer(self, answer: str) -> List:
         return self.tokenizer(answer.replace('.', ''))
@@ -60,6 +87,32 @@ class VisualGenomeQuestionsAnswers(ImageTextDataset):
                 for datapoint in image['qas']
             ]))
 
+    def get_shifting_augmentations(self) -> List[Callable]:
+        onePixelAsPercent = 1 / self.image_size
+
+        return [
+            RandomAffine(degrees=(0, 0), translate=(x * onePixelAsPercent, y * onePixelAsPercent))
+            for y in range(self.patch_size)
+            for x in range(self.patch_size)
+        ]
+
+    def init_augmentations(self) -> List[Callable]:
+        return [
+            *self.get_shifting_augmentations(),
+            RandomHorizontalFlip(p=1),
+            RandomVerticalFlip(p=1),
+            ColorJitter(brightness=(.2, .2)),
+            ColorJitter(brightness=(.3, .3)),
+            ColorJitter(brightness=(.4, .4)),
+            ColorJitter(brightness=(.5, .5)),
+            ColorJitter(brightness=(.6, .6)),
+            ColorJitter(contrast=(.2, .2)),
+            ColorJitter(contrast=(.3, .3)),
+            ColorJitter(contrast=(.4, .4)),
+            ColorJitter(contrast=(.5, .5)),
+            ColorJitter(contrast=(.6, .6)),
+        ]
+
     def load_image(self, index: int) -> Image.Image:
         filename = f"{self.data[index][0]}.jpg"
 
@@ -80,16 +133,22 @@ class VisualGenomeQuestionsAnswers(ImageTextDataset):
         return tensor(self.vocab[self.data[index][2]])
 
     def __getitem__(self, index: int):
-        # transform target word to numeric tensor using vocab
-        answer = self.load_target(index)
+        augmentations_index = index // self.num_images
+        datapoint_index = index % self.num_images
 
-        question = self.questions[index]
+        # transform target word to numeric tensor using vocab
+        answer = self.load_target(datapoint_index)
+
+        question = self.questions[datapoint_index]
 
         # get image tensor
-        image = self.load_image(index)
+        image = self.load_image(datapoint_index)
 
         if self.transform:
             image = self.transform(image)
+
+        # apply augmentations
+        image = self.augmentations[augmentations_index](image)
 
         if isinstance(image, Tensor):
             image = image.float()
@@ -104,4 +163,5 @@ class VisualGenomeQuestionsAnswers(ImageTextDataset):
         return self.questions.shape[1]
 
     def __len__(self) -> int:
-        return len(self.data)
+        return self.num_images * len(self.augmentations)
+
--- main.py
+++ main.py
@@ -1,75 +1,77 @@
 import optuna
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from optuna.trial import Trial
 from json import dumps
 from os.path import exists
 from os import mkdir
 
-from datasets.coco.CocoDataModule import CocoDataModule
 from datasets.visualgenome.VisualGenomeDataModule import VisualGenomeDataModule
 from model.UnifiedTransformer import UnifiedTransformer
 
 
 def objective(trial: Trial) -> float:
+    # fix number of encoder layers at 6
+    num_encoder_layers = 6
+
     # Define the hyper parameters: the numbers represent ranges out of which
     # optuna is going to sample values in a random grid search
-    num_encoder_layers = trial.suggest_int('num_encoder_layers', 6, 18)
-    dropout = trial.suggest_float('dropout', 0.1, 0.5)
+    dropout = trial.suggest_categorical('dropout', [0.1, 0.2, 0.3, 0.5])
 
     # We are testing our model in four different settings:
     # 1. We compare two settings, (a) with a patch embedding and (b) with a
     # convolutional embedding
     has_convolutional_embedding = trial.suggest_categorical('has_convolutional_embedding', [True, False])
-    # 2. We're testing our model on two different datasets: Coco (true/false captions for images)
-    # and Visual Genome (Visual Question Answering)
-    dataset = trial.suggest_categorical('data_module', ['Coco', 'Visual Genome'])
 
-    data_module = CocoDataModule() if dataset == 'Coco' else VisualGenomeDataModule()
+    data_module = VisualGenomeDataModule()
 
-    # create directory for saved models if not exists
-    saved_dir = 'saved'
-    if not exists(saved_dir):
-        mkdir(saved_dir)
-    
-    # log hyper parameters
-    hyper_parameters = dict(
-        num_encoder_layers=num_encoder_layers,
-        dropout=dropout,
-        conv_embed=has_convolutional_embedding,
-        dataset=dataset
-    )
-
-    print("Hyper Parameters: ", hyper_parameters)
-
-    model = UnifiedTransformer(
-        image_size=data_module.image_size,
-        num_tokens=data_module.vocab_size,
-        sequence_length=data_module.sequence_length,
-        num_encoder_layers=num_encoder_layers,
-        dropout=dropout,
-        num_classes=data_module.num_classes,
-        filename=f'{saved_dir}/{dumps(hyper_parameters)}.pt',
-
-        # In the experiment, a patch embedding
-        # will be contrasted to a convolutional
-        # embedding
-        convolutional_embedding=has_convolutional_embedding
-    )
-
-    model.save()
-
-    trainer = Trainer(max_epochs=15, gpus=-1)
-
-    trainer.fit(model, data_module)
-
-    return trainer.callback_metrics["val_acc"].item()
+    # # create directory for saved models if not exists
+    # saved_dir = 'saved'
+    # if not exists(saved_dir):
+    #     mkdir(saved_dir)
+    #
+    # # log hyper parameters
+    # hyper_parameters = dict(
+    #     dropout=dropout,
+    #     conv_embed=has_convolutional_embedding,
+    # )
+    #
+    # print("Hyper Parameters: ", hyper_parameters)
+    #
+    # model = UnifiedTransformer(
+    #     image_size=data_module.image_size,
+    #     num_tokens=data_module.vocab_size,
+    #     sequence_length=data_module.sequence_length,
+    #     num_encoder_layers=num_encoder_layers,
+    #     dropout=dropout,
+    #     num_classes=data_module.num_classes,
+    #     filename=f'{saved_dir}/{dumps(hyper_parameters)}.pt',
+    #
+    #     # In the experiment, a patch embedding
+    #     # will be contrasted to a convolutional
+    #     # embedding
+    #     convolutional_embedding=has_convolutional_embedding
+    # )
+    #
+    # # if a with the given hyper parameters model already exists,
+    # # then we load it
+    # model.load()
+    #
+    # trainer = Trainer(max_epochs=300, gpus=-1, callbacks=[EarlyStopping(monitor="val_loss")])
+    #
+    # trainer.fit(model, data_module)
+    #
+    # return trainer.callback_metrics["val_acc"].item()
 
 
 if __name__ == '__main__':
-    study = optuna.create_study(
-        study_name='multimodal_transformer_study',
-        storage='sqlite:///study.db',
-        load_if_exists=True
-    )
+   data_module = VisualGenomeDataModule()   
 
-    study.optimize(objective, n_trials=30)
+   # study = optuna.create_study(
+   #     study_name='multimodal_transformer_study',
+   #     storage='sqlite:///study.db',
+   #     load_if_exists=True
+   # )
+
+   # study.optimize(objective, n_trials=30)
+
