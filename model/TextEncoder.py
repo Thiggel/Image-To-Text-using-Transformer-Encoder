6955828f9110987058d47630561bd0329c62bfce
F.M.De.Sousa.Horta.Osorio.Laitenberger@student.rug.nl <s3894479@peregrine.hpc.rug.nl>
2022-03-30 11:41:35 +0200

    .

--- model/TextEncoder.py
+++ model/TextEncoder.py
@@ -11,6 +11,11 @@ class TextEncoder(Module):
         self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
     def forward(self, x: str) -> Tensor:
+        print(x.get_device())
         x = self.tokenizer(x, return_tensors="pt", padding=True)
+        print(x.get_device())
 
-        return self.model(**x).last_hidden_state
+        x = self.model(**x).last_hidden_state
+	print(x.get_device())
+
+	return x
